fc72430f90a2e009eaf698a6834b04dafa94a8a7
Rafael <arvenegasd@gmail.com>
2020-03-13 01:09:03 +0100

    showing content with vuejs

--- src/assets/components/main.vue
+++ src/assets/components/main.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="mt-4 mb-4 flex-center">
-        <div class="box card" v-for="(item,index) of items" :key="index">
-            <img class="img-card" src="item.image" alt="Card image cap">
+    <div class="mt-4 mb-4 md-8 flex-center">
+        <div class="box card" v-for="(item, i) of items" :key="i">
+            <img class="img-card" :src="item.image" alt="Card image cap">
             <div class="">
             <div class="dark black-box flex-center">
-                <span>item.title</span>
+                <span>{{item.title}}</span>
             </div>
             <div class="grey grey-box flex-center">
-                <small class=""><b>item.description</b></small>
+                <small class=""><b>{{item.description}}</b></small>
             </div>     
             </div>
             <div class="btn-box flex-center">
@@ -19,10 +19,9 @@
 
 <script>
 export default {
-//    name: "home",
     data() {
         return {
-            items: [
+           items: [
                 { title:"Nutela", description:"10€", image:"https://wouldbechef.be/wp-content/uploads/2019/02/Nutella.jpg"}, 
                 { title:"Nutela", description:"10€", image:"https://wouldbechef.be/wp-content/uploads/2019/02/Nutella.jpg"}, 
                 { title:"Nutela", description:"10€", image:"https://wouldbechef.be/wp-content/uploads/2019/02/Nutella.jpg"}, 
