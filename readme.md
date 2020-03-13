c7d991d216034f5ae5cf05a6a4f586f7f8a18298
Rafael <arvenegasd@gmail.com>
2020-03-13 12:53:36 +0100

    readme

--- readme.md
+++ readme.md
@@ -2,38 +2,32 @@
 Slash is a part of a tecnical test for SlashMobility. This is a gallery of products in VueJS. 
 
 ## Install/Use
-1. Para ejecutar este proyecto, una vez clonado el repositorio necesitas inicializar NPM
+1. To run this project, once the repository is cloned you need to initialize NPM
 
-NOTA: Debes tener descargado [NPM](https://www.npmjs.com/get-npm)
+NOTE: You must have downloaded [NPM](https://www.npmjs.com/get-npm)
 
-2. Inicializamos el NPM con los valores por defecto
+2. We initialize the NPM with the default values
 ```$npm init -y```
 
-3. A continuación instalamos todas las dependencias a ejecutarse en el proyecto. 
+3. Next we install all the dependencies to run in the project.
 ```$npm install```
 
-4. Cargamos el servidor (JSON SERVER) que creará un fake rest API con el archivo JSON a utilizar en el proyecto. Para esto ejecutamos el comando 
-
-```json-server --watch photos.json```
-
-4. Para que NPM ejecute webpack y construya los archivos necesarios para ejecutar el proyecto ejecutamos: 
+4. For NPM to run webpack and build the necessary files to run the project we run:
 
 ```npm run start```
 
 ## Tecnologías utilizadas
 - [ ] 1. VueJs
-- [ ] 2. Axios
-- [ ] 3. Json Server
-- [ ] 4. SASS
-- [ ] 5. Webpack
-- [ ] 6. NPM
+- [ ] 2. SASS
+- [ ] 3. Webpack
+- [ ] 4. NPM
 
-## Contribuciones
-Pull requests son bienvenidas. Para sugerencias y cambios mayores por favor contáctese al github para discutir sobre los cambios. 
-Por favor, relizar los test que consideren necesarios. 
+## Contributions
+Pull requests are welcome. For suggestions and major changes please contact the github to discuss the changes.
+Please, carry out the tests that you consider necessary.
 
 ## About the project
-https://github.com/rafaelvenegasd/vuejs.git
+https://github.com/rafaelvenegasd/slash.git
 
 ## The genius behind this
 https://github.com/rafaelvenegasd
--- src/components/main.vue
+++ src/components/main.vue
@@ -1,34 +1,45 @@
 <template>
-    <div class="mt-4 mb-4 flex-center container">
+    <div class="mt-4 mb-4 d-flex center font-bold container">
         <div class="row container">
             <div class="box col-sm-3 col-md-5 col-lg-3" v-for="(item, i) of items" :key="i">
                 <img class="img-card" :src="item.image" alt="Card image cap">
-                <div class="dark black-box flex-center">
+                <div class="bg-black white black-box d-flex center ">
                     <span>{{item.title}}</span>
                 </div>
-                <div class="grey grey-box flex-center">
+                <div class="grey grey-box d-flex center font-bold">
                     <small class=""><b>{{item.description}}</b></small>
                 </div>     
-                <div class="btn-box flex-center">
+                <div class="btn-box bg-black d-flex center font-bold">
                     <a class="link" type="button" id="show-modal" @click="showModal()">Buy</a>
                 </div>      
             </div>
         </div>
-        <div v-if="modal">
-            <div class="mt-4 mb-4 flex-center container">
-                <div class="row container">
-                    <div>    
-                        <div class="btn-box">
-                            <a class="link" type="button" id="show-modal" @click="showModal()">Close</a>
-                        </div>      
+        <div v-if="modal" class="modal-mask center d-flex">
+            <div class="modal-wrapper center d-flex">
+                <div class="modal-container center font-bold">
+                    <div class="modal-header center d-flex justify-content-center">
+                        <slot name="header">
+                        Do you want to buy it?
+                        </slot>
+                    </div>
+                    <div class="modal-body justify-content-center col-sm-12">
+                        <slot name="body" class="d-block">
+                            <input class="form-control" type="text" name="username" id="username" placeholder="Username">
+                            <input class="form-control" type="password" name="password" id="password" placeholder="Password">
+                        </slot>
+                    </div>
+                    <div class="d-flex without-borders">
+                        <div class="d-flex center font-bold grey footer-btn without-borders">
+                            <a class="link" type="button" id="show-modal" @click="showModal()">Cancel</a>
+                        </div>
+                        <div class="d-flex center font-bold btn-box bg-black footer-btn without-borders">
+                            <a class="link " type="button" id="show-modal" @click="showModal()">Confirm</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
-
-
     
 </template>
 
@@ -58,11 +69,8 @@ export default {
             }else{
                 this.modal = false
             }
-
         }
     }
 }
 
-
 </script>
-  
